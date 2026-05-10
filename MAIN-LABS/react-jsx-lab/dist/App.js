1e0aa2f9a592c433f960cf94aaab227090e1c3d3
Zedekiah Heteroza <zedekiahheteroza@gmail.com>
2026-05-10 18:53:29 +0800

    Add react-jsx-lab as 11th Main Lab

---
+++ MAIN-LABS/react-jsx-lab/dist/App.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _Item = _interopRequireDefault(require("./Item"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function App() {
+  var items = ['Item 1', 'Item 2', 'Item 3'];
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "My Items:"), /*#__PURE__*/_react["default"].createElement("ul", null, items.map(function (item, index) {
+    return /*#__PURE__*/_react["default"].createElement(_Item["default"], {
+      key: index,
+      name: item
+    });
+  })));
+}
+var _default = exports["default"] = App;
